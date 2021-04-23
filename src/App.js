70d4c91f2f5119e56ed01fc451e8769044738830
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-04-23 13:35:48 +0300

    combine css files. App.css is main now

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './index.css';
-import './style.css'
+import './App.css'
 import Header from './components/Header';
 import Contacts from "./components/Contacts"
 
