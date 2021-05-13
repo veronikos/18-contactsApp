80f10248552e06f8d5844f1e34767c4a718f4590
Veronika_Kostenko_R <veronikostenko@gmail.com>
2021-05-13 13:36:17 +0300

    make mentor changes

--- src/components/Contacts.js
+++ src/components/Contacts.js
@@ -1,66 +1,75 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import Person from "./Person";
-import Search from "./Search";
+
+let contacts = [
+  {
+    firstName: "Барней",
+    lastName: "Стинсовський",
+    phone: "+380956319521",
+    gender: "male",
+  },
+  {
+    firstName: "Робін",
+    lastName: "Щербатська",
+    phone: "+380931460123",
+    gender: "female",
+  },
+  {
+    firstName: "Анонімний",
+    lastName: "Анонімус",
+    phone: "+380666666666",
+  },
+  {
+    firstName: "Лілія",
+    lastName: "Олдровна",
+    phone: "+380504691254",
+    gender: "female",
+  },
+  {
+    firstName: "Маршен",
+    lastName: "Еріксонян",
+    phone: "+380739432123",
+    gender: "male",
+  },
+  {
+    firstName: "Теодор",
+    lastName: "Мотсбес",
+    phone: "+380956319521",
+    gender: "male",
+  },
+];
 
 class Contacts extends Component {
-  
   state = {
-    contacts: [{
-      firstName: "Барней",
-      lastName: "Стинсовський",
-      phone: "+380956319521",
-      gender: "male"
-    }, {
-      firstName: "Робін",
-      lastName: "Щербатська",
-      phone: "+380931460123",
-      gender: "female"
-    }, {
-      firstName: "Анонімний",
-      lastName: "Анонімус",
-      phone: "+380666666666"
-    }, {
-      firstName: "Лілія",
-      lastName: "Олдровна",
-      phone: "+380504691254",
-      gender: "female"
-    }, {
-      firstName: "Маршен",
-      lastName: "Еріксонян",
-      phone: "+380739432123",
-      gender: "male"
-    }, {
-      firstName: "Теодор",
-      lastName: "Мотсбес",
-      phone: "+380956319521",
-      gender: "male"
-    }],
-    filteredContacts: [],
-    search: ""
-  }
+    contacts: contacts,
+    search: "",
+  };
 
-  addSearchName = data => {
-    this.setState({search: data})
-    const filtered = this.state.contacts
-    .filter((contact) =>
-      contact.lastName.toLowerCase().startsWith(data.toLowerCase()) ||
-      contact.firstName.toLowerCase().startsWith(data.toLowerCase()) ||
-      contact.phone.includes(data))  
-
-      this.setState({filteredContacts: filtered})
-  }
+  handleSearchChange = (e) => {
+    const inputValue = e.target.value.toLowerCase();
+    this.setState({
+      search: e.target.value,
+      contacts: contacts.filter(
+        (contact) =>
+          contact.lastName.toLowerCase().includes(inputValue) ||
+          contact.firstName.toLowerCase().includes(inputValue) ||
+          contact.phone.includes(inputValue)
+      ),
+    });
+  };
 
   render() {
-
-    let filteredContacts = this.state.search === "" ? this.state.contacts : this.state.filteredContacts
-    console.log(this.state.search)
     return (
       <div className="Contacts">
-        <Search addSearchName={this.addSearchName} />
-        { filteredContacts
-          .map((person, i) => (
-            <Person {...person} key={i} />
-          ))}
+        <input
+          type="text"
+          placeholder="Search contacts..."
+          value={this.state.search}
+          onChange={this.handleSearchChange}
+        />
+        {this.state.contacts.map((person, i) => (
+          <Person {...person} key={i} />
+        ))}
       </div>
     );
   }
